dcd46a7517e5e600ebac97a0049dc9e0dbf5e08e
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-11-15 14:15:06 -0800

    added more docs

--- append_rune.go
+++ append_rune.go
@@ -7,6 +7,17 @@ import (
 )
 
 
+// AppendRune appends the string-literal-encoding of a rune to the []byte `p`.
+//
+// Many runes stay as the are.
+//
+// For example, rune(55), rune(65), and rune(100) would stay as is, since they
+// are the runes '7', 'A' and 'd' respectively.
+//
+// However, some runes change.
+//
+// For example, rune(13), rune(21), rune(27), and rune(92) would change, since they are
+// the rune runes '\r', '\x15', '\x1b', and '\\' respectively.
 func AppendRune(p []byte, r rune) []byte {
 
 	if r > utf8.MaxRune {
