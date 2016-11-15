9e7ed68b214c3aadf0737e326d6c19a029665013
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-11-15 12:11:24 -0800

    added docs

--- doc.go
+++ doc.go
@@ -5,17 +5,26 @@ For example, rune value 10 (which is the "line feed" character) becomes \n
 
 Also, for example, rune value 31 (which is the "unit separator" character) becomes \x1f
 
-Sample Code Using AppendRune
+Sample Code
 
-	p := []byte{}
 	
 	// ...
 	
-	r := rune(11) // This is the Vertical Tab character. Which could also be written '\v'
+	r1 := rune(11) // This is the Vertical Tab character. Which could also be written '\v'
+	r2 := rune(84) // This is the Uppercase T character. Which could also be written 'T'
+	r2 := rune(30) // This is the Row Separator character. Which could also be written '\x1e'
 	
-	p = inquote.AppendRune(p, r)
+	p := []byte{}
 	
+	p = inquote.AppendRune(p, r1)
 	// p == []byte{ rune(92), rune(118) } == []byte{'\\', 'v'}
+	
+	p = inquote.AppendRune(p, r2)
+	// p == []byte{ rune(92), rune(118), rune(84) } == []byte{'\\', 'v', 'T'}
+	
+	p = inquote.AppendRune(p, r3)
+	// p == []byte{ rune(92), rune(118), rune(84), rune(92), rune(120), rune(49), rune(101) } == []byte{'\\', 'v', 'T', '\\', 'x', '1', 'e'}
+
 
 */
 package inquote
